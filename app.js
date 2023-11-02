0a4ce84f2481dff41e16ed44db836dbbc2525280
richard579 <richardbeng358@outlook.com>
2023-11-02 09:15:46 -0700

    discover_data.json

--- app.js
+++ app.js
@@ -1,71 +1,59 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Mock JSON data (replace this with your actual API call or data source)
-    const mockData = {
-        "recommendations": [
-            {
-                "playlist": {
-                    "id": 1,
-                    "title": "Chill Vibes",
-                    "description": "Relaxing tunes for a laid-back day.",
-                    "cover": "https://example.com/covers/chill_vibes.jpg"
-                }
-            },
-            {
-                "album": {
-                    "id": 1,
-                    "title": "Rhythmic Beats",
-                    "artist": "The Melody Makers",
-                    "cover": "https://example.com/covers/rhythmic_beats.jpg"
-                }
-            },
-            {
-                "track": {
-                    "id": 1,
-                    "title": "Sunset Serenade",
-                    "artist": "Acoustic Ensemble",
-                    "album": "Sounds of Nature",
-                    "cover": "https://example.com/covers/sunset_serenade.jpg"
-                }
-            },
-            // Include more items as needed...
-        ]
-    };
-
-    // Access the recommendations container in the HTML
     const recommendationsContainer = document.getElementById("recommendations-container");
 
-    // Function to create HTML elements based on item type
-    function createHTML(item) {
-        let newElement = document.createElement("div");
-        if (item.playlist) {
-            newElement.innerHTML = `
-                <div class="playlist">
-                    <img src="${item.playlist.cover}" alt="${item.playlist.title}">
-                    <h3>${item.playlist.title}</h3>
-                    <p>${item.playlist.description}</p>
-                </div>
-            `;
-        } else if (item.album) {
-            newElement.innerHTML = `
-                <div class="album">
-                    <img src="${item.album.cover}" alt="${item.album.title}">
-                    <h3>${item.album.title} by ${item.album.artist}</h3>
-                </div>
-            `;
-        } else if (item.track) {
-            newElement.innerHTML = `
-                <div class="track">
-                    <img src="${item.track.cover}" alt="${item.track.title}">
-                    <h3>${item.track.title} by ${item.track.artist}</h3>
-                </div>
-            `;
-        }
-        return newElement;
+    // Fetch the JSON data
+    fetch('discovery_data.json')
+        .then(response => response.json())
+        .then(data => {
+            data.recommendations.forEach(item => {
+                let newElement;
+                if (item.playlist) {
+                    newElement = createPlaylistElement(item.playlist);
+                } else if (item.album) {
+                    newElement = createAlbumElement(item.album);
+                } else if (item.track) {
+                    newElement = createTrackElement(item.track);
+                }
+
+                recommendationsContainer.appendChild(newElement);
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching the data:', error);
+        });
+
+    // Function to create HTML elements for playlist
+    function createPlaylistElement(playlist) {
+        const playlistElement = document.createElement("div");
+        playlistElement.classList.add("playlist");
+        playlistElement.innerHTML = `
+            <img src="${playlist.cover}" alt="${playlist.title}">
+            <h3>${playlist.title}</h3>
+            <p>${playlist.description}</p>
+        `;
+        return playlistElement;
     }
 
-    // Iterate through the recommendations and append HTML elements to the container
-    mockData.recommendations.forEach(item => {
-        const newElement = createHTML(item);
-        recommendationsContainer.appendChild(newElement);
-    });
+    // Function to create HTML elements for album
+    function createAlbumElement(album) {
+        const albumElement = document.createElement("div");
+        albumElement.classList.add("album");
+        albumElement.innerHTML = `
+            <img src="${album.cover}" alt="${album.title}">
+            <h3>${album.title} by ${album.artist}</h3>
+        `;
+        return albumElement;
+    }
+
+    // Function to create HTML elements for track
+    function createTrackElement(track) {
+        const trackElement = document.createElement("div");
+        trackElement.classList.add("track");
+        trackElement.innerHTML = `
+            <img src="${track.cover}" alt="${track.title}">
+            <h3>${track.title} by ${track.artist}</h3>
+        `;
+        return trackElement;
+    }
 });
+
